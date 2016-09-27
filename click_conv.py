c386ddb43e0d17fbf1c938db49f808290ca7b9a9
insomniaski <243494752@qq.com>
2016-09-27 18:19:49 +0800

    WANGYQ

--- click_conv.py
+++ click_conv.py
@@ -2,17 +2,27 @@
 import  requests
 import time
 import random
+import logging
 
-#base_url="http://eu.atracking.appflood.com"
-base_url="http://pre.service.aflt.papayamobile.com"
+logging.basicConfig(
+                format='%(asctime)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='click_convertion.log',
+                filemode='a')
+
+#base_url="http://auto.atracking.appflood.com"
+#base_url="http://pre.service.aflt.papayamobile.com"
+base_url="http://54.67.94.143:9092"#dynamic
 #base_url="http://54.215.117.231:1256"
 #设置基地址
 #aff_list = ["21", "883", "1416", "1420", "1459"]
-aff_list=["1435","1482","1458","1429","1400","1032","1481","700","25","1457","1452"]
-
+#aff_list=["1435","1482","1458","1429","1400","1032","1481","700","25","1457","1452"]
+#aff_list=["1500","1501","1502","1503","1504","1505","1506"]
+aff_list=["1458","1468"]
+current_time=time.strftime('%Y%m%d_%H%M%S',time.localtime(time.time()))
 def click_conv(index,aff_id):
-
-    click_url = base_url+"/transaction/post_click?offer_id=112544&aff_id="+aff_id+"&aff_sub=SUB1&aff_sub2=SUB2&aff_sub3=SUB3&aff_sub4=SUB4&aff_sub5=SUB5&source=deduct&aff_sub6=testtime"+str(index)
+    sub_str=str("%03d"%(index+1))
+    click_url = base_url+"/transaction/post_click?offer_id=85168&aff_id="+aff_id+"&aff_sub=SUB1&aff_sub2=SUB2&aff_sub3=SUB3&aff_sub4=SUB4&source=deduct&aff_sub6=test"+sub_str+"&aff_sub5="+current_time
     #测试环境offer_id=5&aff_id=7，线上环境offer_id=317&aff_id=129
     click_res = requests.post(click_url,allow_redirects=False).content#发起点击
     print click_res
@@ -32,12 +42,19 @@ def click_conv(index,aff_id):
     event_res=requests.post(event_url,allow_redirects=False).content#产生事件
     print event_res
 
+    log_text=sub_str+'\t'+aff_id+'\t'+tran_id
+    logging.warning(log_text)
 
-for i in range(100):#设置循环次数
+logging.warning('mission start')
+for i in range(20):#设置循环次数
+    #print len(aff_list)
 
-    aff_id=aff_list[random.randint(0,10)]
-    print '第%d次请求' % (i + 1)
-    #print "aff_id:" + aff_id
+    aff_id=aff_list[random.randint(0,len(aff_list)-1)]
+    print '第%03d次请求' % (i + 1)
+    #log_text= "aff_id:" + aff_id
+    #logging.debug('log_text')
 
+    #print ("%03d"%(i+1))
     click_conv(i,aff_id)
+logging.warning('mission complete')
 
--- selenium_automation/offer_create.py
+++ selenium_automation/offer_create.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 browser = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')
 #browser.get("http://192.168.41.130:3211/")
-browser.get("http://aflt-frontend-pre-1017451597.us-west-1.elb.amazonaws.com")
+browser.get("http://54.67.94.143:9091/")
+#browser.get("http://aflt-frontend-pre-1017451597.us-west-1.elb.amazonaws.com")
 #鼠标不要放在浏览器窗口里!
 #鼠标不要放在浏览器窗口里!!
 #鼠标不要放在浏览器窗口里!!!
@@ -27,11 +28,11 @@ browser.find_element_by_xpath('//*[@id="offer_create_form"]/h4[3]/span').click()
 time.sleep(1)
 browser.find_element_by_xpath('//*[@id="offer_create_form"]/h4[4]/span').click()#点击settings
 browser.find_element_by_xpath('//*[@id="details_group"]/li[1]/select').click()
-browser.find_element_by_xpath('//*[@id="details_group"]/li[1]/select/option[6]').click()#选择advertiser
-browser.find_element_by_xpath('//*[@id="details_group"]/li[2]/input').send_keys('selenium create decuct offer')#offer name
+browser.find_element_by_xpath('//*[@id="details_group"]/li[1]/select/option[7]').click()#选择advertiser
+browser.find_element_by_xpath('//*[@id="details_group"]/li[2]/input').send_keys('_selenium create dynamic offer')#offer name
 browser.find_element_by_xpath('//*[@id="details_group"]/li[3]/textarea').send_keys('test description')#description
 browser.find_element_by_xpath('//*[@id="details_group"]/li[4]/div/input').send_keys('www.baidu.com')#preview url
-browser.find_element_by_xpath('//*[@id="details_group"]/li[5]/div/input').send_keys('www.xiami.com/transaction_id={transaction_id}')#click url
+browser.find_element_by_xpath('//*[@id="details_group"]/li[5]/div/input').send_keys('www.xiami2.com/transaction_id={transaction_id}')#click url
 browser.find_element_by_xpath('//*[@id="revenue_group"]/li[3]/div/div[1]/input[1]').send_keys('.2')#输入价格
 browser.find_element_by_xpath('//*[@id="payout_group"]/li[3]/div/input').send_keys('.1')#输入价格
 browser.find_element_by_xpath('//*[@id="settings_group"]/li[2]/select').click()
@@ -42,17 +43,17 @@ time.sleep(10)
 alert=browser.switch_to.alert
 alert.accept()
 print browser.current_url
-browser.find_element_by_xpath('//*[@id="content"]/div/button').click()
+#browser.find_element_by_xpath('//*[@id="content"]/div/button').click()
 browser.maximize_window()
-time.sleep(3)
-offer_id=browser.find_element_by_xpath('//*[@id="panel_body_details"]/div/div[1]/p[1]').text#获取offer_id
-browser.find_element_by_xpath('//*[@id="content"]/div/div/div[1]/div[1]/div[1]/span[2]/a[1]').click()#点击edit
-time.sleep(3)
+#time.sleep(3)
+#offer_id=browser.find_element_by_xpath('//*[@id="panel_body_details"]/div/div[1]/p[1]').text#获取offer_id
+#browser.find_element_by_xpath('//*[@id="content"]/div/div/div[1]/div[1]/div[1]/span[2]/a[1]').click()#点击edit
+#time.sleep(3)
 #browser.find_element_by_xpath('//*[@id="offer_create_form"]/ul/li[2]/input').clear()
-browser.find_element_by_xpath('//*[@id="offer_create_form"]/ul/li[2]/input').send_keys(offer_id[4:])#修改offer name
-browser.find_element_by_xpath('//*[@id="offer_create_form"]/button').click()
-time.sleep(5)
-alert=browser.switch_to.alert
-alert.accept()
-time.sleep(3)
+#browser.find_element_by_xpath('//*[@id="offer_create_form"]/ul/li[2]/input').send_keys(offer_id[4:])#修改offer name
+#browser.find_element_by_xpath('//*[@id="offer_create_form"]/button').click()
+#time.sleep(5)
+#alert=browser.switch_to.alert
+#alert.accept()
+#time.sleep(3)
 browser.execute_script("alert('selenium complete')")
